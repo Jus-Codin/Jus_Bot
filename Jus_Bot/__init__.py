00425bd69d8b8bb7baec9db40d4b950438a0426e
Jus-Codin <justajusgamin@gmail.com>
2021-05-17 11:03:06 +0800

    Update __init__.py

--- Jus_Bot/__init__.py
+++ Jus_Bot/__init__.py
@@ -1,10 +1,10 @@
-from discord import client
+from discord import Client
 from .base import JusBotBase
 from .Web import open_web
 
 __version__ = '3.1.6'
 
-class Jus_Bot(JusBotBase, client):
+class Jus_Bot(JusBotBase, Client):
   '''Main bot with modified BotBase'''
 
   pass
